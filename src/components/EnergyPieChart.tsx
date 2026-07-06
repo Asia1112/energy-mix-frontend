46b1b67860795d843c3785d9f205f67c0cac8c13
Asia1112 <asia1411@op.pl>
2026-07-06 18:24:55 +0200

    Disable pie chart animation

--- src/components/EnergyPieChart.tsx
+++ src/components/EnergyPieChart.tsx
@@ -78,6 +78,7 @@ export function EnergyPieChart({
               outerRadius={76}
               label={renderPieLabel}
               labelLine={false}
+              isAnimationActive={false}
               onClick={(entry) => {
                 if (entry.name) {
                   onFuelSelect(entry.name);
